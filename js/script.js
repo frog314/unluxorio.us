13bbfd7acdd1bfda741656b881602b33b8a4bbe2
Ember314 <ember314dis@gmail.com>
2022-11-20 21:21:54 -0500

    oh my god the blur is so good
    if the blur was somehow a living creature I would 10/10 sex

--- js/script.js
+++ js/script.js
@@ -1,3 +1,50 @@
-function cool(){
-    window.location="https://google.com/";
+
+function shop(){
+    window.location="/shop.html";
 }
+
+function faq(){
+    window.location="/faq.html";
+}
+
+function home(){
+    window.location="/index.html";
+}
+
+
+/*
+function getMouseCoords(e) {
+    var e = e || window.event;
+    document.getElementById('container').innerHTML = e.clientX + ', ' +
+      e.clientY + '<br>' + e.screenX + ', ' + e.screenY;
+  }
+  
+  
+  var followCursor = (function() {
+    var s = document.createElement('div');
+    s.style.position = 'absolute';
+    s.style.margin = '0';
+    s.style.padding = '5px';
+    s.style.pointerEvents = 'none';
+    s.textContent = "🚀"
+
+  
+    return {
+      init: function() {
+        document.body.appendChild(s);
+      },
+  
+      run: function(e) {
+        var e = e || window.event;
+        s.style.left = (e.clientX - 5) + 'px';
+        s.style.top = (e.clientY - 5) + 'px';
+        getMouseCoords(e);
+      }
+    };
+  }());
+  
+  window.onload = function() {
+    followCursor.init();
+    document.body.onmousemove = followCursor.run;
+  }
+  */
